39682a14432727bd3f17d8b30b513ede9082a726
bonillab <nestorbonillabrenes@gmail.com>
2019-06-19 09:18:48 -0600

    get status from the database

---
+++ db/tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE xark_status(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	create_at INTEGER,
+	sync_status BOOLEAN,
+	collect_status BOOLEAN,
+	sync_date DATETIME,
+	collect_date DATETIME
+);
+
+CREATE TABLE data_xo(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	serial VARCHAR,
+	uuid VARCHAR,
+	create_at DATETIME,
+	update_at DATETIME
+);
--- xark.py
+++ xark.py
@@ -12,35 +12,67 @@ import sys
 import time
 import sched
 import logging
-import datetime
+import datetime as dt
 import subprocess
+import sqlite3
 
 # Logging setting
 logger = logging.getLogger('xark')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)-s - %(message)s')
-handler = logging.FileHandler(filename='/var/log/xark.log', mode='a')
+handler = logging.FileHandler(filename='xark.log', mode='a')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-
-class Xark():
-
+class conexion():
     def __init__(self):
+        self.conn = sqlite3.connect('main.db')
+        self.c = self.conn.cursor()
+
+    def get(self, query):
+        return self.c.execute(query).fetchone()
+        self.c.close()
+        self.conn.close()
+
+    def set(self, query, data):
+        self.c.executemany(query, data)
+        self.conn.commit()
+        self.c.close()
+        self.conn.close()
+
+class Xark(conexion):
+    def __init__(self):
+        self.db = conexion()
         # Estado de sincronizacion en `No Sincronizado`
-        self.sync_status = False
-        # Estado de recoleccion en `No Recolectado`
-        self.collec_status = False
+        # self.sync_status = False
+        # query = self.db.get("SELECT sync_status, collect_status FROM xark_status WHERE create_at={0}".format(dt.datetime.now().date()))
+
+        try:
+            self.sync_status = query[0]
+            # Estado de recoleccion en `No Recolectado`
+            self.collec_status = query[1]
+        except:
+            self.sync_status = False
+            # Estado de recoleccion en `No Recolectado`
+            self.collec_status = False
+        # self.collec_status = False
         self.s = sched.scheduler(time.time, time.sleep)
 
     def getSerialNumber(self):
         '''Capturar Numero de serie y UUID'''
         data = dict()
-        f = open('/home/.devkey.html', 'r')
+        f = open('../.devkey.html', 'r')
         for value in f:
             if 'serialnum' in value or 'uuid' in value:
                 value = value.strip().split(' ')
                 data[value[2].split('=')[1].replace('"', '')] = value[3].split('=')[1].replace('"', '')
+        print(data)
+
+        try:
+            self.db.set("INSERT INTO data_xo(serial, uuid, create_at, update_at)VALUES(?,?,?,?)", [(data['serialnum'], data['uuid'], dt.datetime.now(), dt.datetime.now())])
+        except sqlite3.IntegrityError as e:
+            print(e)
+
         return data
 
     def collection(self):
@@ -49,10 +81,11 @@ class Xark():
             # Termina la funcion ya se ha recolectado informacion
             return self.collec_status
 
+        print("recolecta")
         data = self.getSerialNumber()
         # Estado de sincronizacion en `Sincronizado`
-        self.collec_status = True
 
+        self.collec_status = True
         return data
 
     def synchrome(self, data):
@@ -67,6 +100,7 @@ class Xark():
             stdout=subprocess.PIPE
         ).stdout.readlines()
         code = int(code[0].strip())
+
         if code == 200:
             url = 'http://10.0.11.33:5000/data/{0}/{1}'.format(data['serialnum'], data['uuid'])
             result = subprocess.Popen(
@@ -74,6 +108,7 @@ class Xark():
                 shell=True,
                 stdout=subprocess.PIPE
             ).stdout.readlines()
+
             print(result[0].strip())
             result = int(result[0].strip())
             if code == 200:
@@ -88,14 +123,14 @@ class Xark():
 if __name__ == '__main__':
     try:
         # Log de inicio diario
-        logger.info('Inicio de dia {}'.format(datetime.datetime.now()))
+        logger.info('Inicio de dia {}'.format(dt.datetime.now()))
         # Instancia del Kaibil
         xark = Xark()
 
         # Verifica si el dia de la semana es entre lunes y viernes
-        if datetime.datetime.now().weekday() > 1 and datetime.datetime.now().weekday() < 5:
+        if dt.datetime.now().weekday() >= 1 and dt.datetime.now().weekday() <= 5:
             # Verifica que la hora del dia sea entre las 6:00 y las 18:00
-            if datetime.datetime.now().time() > datetime.time(6, 0) and datetime.datetime.now().time() < datetime.time(18, 0):
+            if dt.datetime.now().time() >= dt.time(6, 0) and dt.datetime.now().time() <= dt.time(18, 0):
                 # Recolectar informacion
                 data = xark.collection()
                 # sincronizar con el charco
