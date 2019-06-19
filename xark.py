34ea956c938e242e7893eb80d423436205bd9c24
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-06-19 13:24:47 -0600

    black formater

--- xark.py
+++ xark.py
@@ -12,9 +12,9 @@ import sys
 import time
 import sched
 import logging
-import datetime as dt
-import subprocess
 import sqlite3
+import datetime
+import subprocess
 import multiprocessing
 
 
@@ -27,7 +27,7 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 
-class conexion():
+class conexion:
     def __init__(self):
         self.conn = sqlite3.connect('main.db')
         self.c = self.conn.cursor()
@@ -45,18 +45,21 @@ class conexion():
 
 
 class Xark(conexion):
-
     def __init__(self):
         self.db = conexion()
         # Estado de sincronizacion en `No Sincronizado`
         # self.sync_status = False
-        query = self.db.get('SELECT sync_status, collect_status FROM xark_status WHERE create_at={0}'.format(dt.datetime.now().date()))
+        query = self.db.get(
+            'SELECT sync_status, collect_status FROM xark_status WHERE create_at={0}'.format(
+                datetime.datetime.now().date()
+            )
+        )
 
         try:
             self.sync_status = query[0]
             # Estado de recoleccion en `No Recolectado`
             self.collec_status = query[1]
-        except():
+        except ():
             self.sync_status = False
             # Estado de recoleccion en `No Recolectado`
             self.collec_status = False
@@ -66,14 +69,26 @@ class Xark(conexion):
     def getSerialNumber(self):
         '''Capturar Numero de serie y UUID'''
         data = dict()
-        f = open('../.devkey.html', 'r')
+        f = open('/home/.devkey.html', 'r')
         for value in f:
             if 'serialnum' in value or 'uuid' in value:
                 value = value.strip().split(' ')
-                data[value[2].split('=')[1].replace('"', '')] = value[3].split('=')[1].replace('"', '')
+                data[value[2].split('=')[1].replace('"', '')] = (
+                    value[3].split('=')[1].replace('"', '')
+                )
 
         try:
-            self.db.set('INSERT INTO data_xo(serial, uuid, create_at, update_at)VALUES(?,?,?,?)', [(data['serialnum'], data['uuid'], dt.datetime.now(), dt.datetime.now())])
+            self.db.set(
+                'INSERT INTO data_xo(serial, uuid, create_at, update_at)VALUES(?,?,?,?)',
+                [
+                    (
+                        data['serialnum'],
+                        data['uuid'],
+                        datetime.datetime.now(),
+                        datetime.datetime.now(),
+                    )
+                ],
+            )
         except sqlite3.IntegrityError as e:
             print(e)
 
@@ -85,7 +100,7 @@ class Xark(conexion):
             # Termina la funcion ya se ha recolectado informacion
             return self.collec_status
 
-        print("recolecta")
+        print('recolecta')
         data = self.getSerialNumber()
         # Estado de sincronizacion en `Sincronizado`
 
@@ -98,20 +113,22 @@ class Xark(conexion):
             # Termina la funcion si ya se a sincronizacion con el charco
             return self.sync_status
         # Verifica si el IIAB esta disponible
-        print('Verificacion a : {}'.format(dt.datetime.now()))
+        print('Verificacion a : {}'.format(datetime.datetime.now()))
         code = subprocess.Popen(
             'curl -o /dev/null -s -w "%{http_code}\n" http://10.0.11.33:5000/',
             shell=True,
-            stdout=subprocess.PIPE
+            stdout=subprocess.PIPE,
         ).stdout.readlines()
         code = int(code[0].strip())
 
         if code == 200:
-            url = 'http://10.0.11.33:5000/data/{0}/{1}'.format(data['serialnum'], data['uuid'])
+            url = 'http://10.0.11.33:5000/data/{0}/{1}'.format(
+                data['serialnum'], data['uuid']
+            )
             result = subprocess.Popen(
                 'curl -o /dev/null -s -w "%{http_code}\n" ' + url,
                 shell=True,
-                stdout=subprocess.PIPE
+                stdout=subprocess.PIPE,
             ).stdout.readlines()
 
             print(result[0].strip())
@@ -127,7 +144,7 @@ class Xark(conexion):
 
 if __name__ == '__main__':
     # Log de inicio diario
-    logger.info('Inicio de dia {}'.format(dt.datetime.now()))
+    logger.info('Inicio de dia {}'.format(datetime.datetime.now()))
     # Contexto paralelo para multiprocesos.
     context = multiprocessing.get_context('spawn')
     # Cola de salida de cada proceso.
@@ -139,9 +156,14 @@ if __name__ == '__main__':
         xark = Xark()
 
         # Verifica si el dia de la semana es entre lunes y viernes
-        if dt.datetime.now().weekday() >= 1 and dt.datetime.now().weekday() <= 5:
+        if (
+            datetime.datetime.now().weekday() >= 1
+            and datetime.datetime.now().weekday() <= 5
+        ):
             # Verifica que la hora del dia sea entre las 6:00 y las 18:00
-            if dt.datetime.now().time() >= dt.time(6, 0) and dt.datetime.now().time() <= dt.time(18, 0):
+            if datetime.datetime.now().time() >= datetime.time(
+                6, 0
+            ) and datetime.datetime.now().time() <= datetime.time(18, 0):
                 # Recolectar informacion
                 processes.append(context.Process(target=xark.collection, args=()))
                 # Sincronizar con el charco
