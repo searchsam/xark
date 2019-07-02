7e8a8af1c18d1861959f58d9c40f7abdd0021ea8
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-07-01 20:30:20 -0600

    Commento all the code

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/#!/bin/sh
 
+# Crea la base datos para sqlite3 limpia
 if [ -f ./xark.db ];
 then
     # Drop old database sqlite
@@ -10,8 +11,41 @@ else
     cat ./db/tables.sql | sqlite3 ./xark.db
 fi
 
+# Eliminar el archivo log
 if [ -f ./xark.log ];
 then
     # Drop old log file
     rm ./xark.log
+fi
+
+# Crea la unidad de servicios
+if [ -f ./xarkd.service ];
+then
+    # Drop old log file
+    rm ./xarkd.service
+    echo "[Unit]
+Description=Put kaibil to work
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=.
+ExecStart=/usr/bin/python ./xark.py
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target" > xarkd.service
+else
+    echo "[Unit]
+Description=Put kaibil to work
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=.
+ExecStart=/usr/bin/python ./xark.py
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target" > xarkd.service
 fi
